eaace9c6c0c1cb62ad676f9467c825311adc69fb
krishnakumarik <krishnakumarikondapuram@gmail.com>
2021-09-27 20:12:12 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'roundone';
+  value = "";
+  name = "";
+
+  colors = [
+    { key: "green", value: 1 },
+    { key: "red", value: 1 },
+    { key: "yellow", value: 1 },
+    { key: "black", value: 1 },
+    { key: "white", value: 1 }
+  ]
+  colorValue = ""
+  constructor(){
+    
+  }
+  delete = () => {
+    this.colors.find(x => {
+      if (x.key == this.colorValue) {
+        x.value = 0;
+      }
+    });
+    this.colors= this.colors.filter(x=>x.value == 1)
+
+  }
+
+
+
 }
